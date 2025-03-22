57bc23313afee3cef4a9443cc6e2a20d89763c5b
Stanislav Antonenko <djak741steam2@mail.ru>
2025-03-23 00:16:32 +0300

    WIP: generate array, basic validations, designated tests

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,33 @@
-console.log('asdasdasd')
+export function serialize(arr: number[]) : string{
+    return arr.join('\s');
+}
+
+export function deserialize(str: string) : number[] {
+    return str.split('\s').map(el => parseInt(el));
+}
+
+export function createArray(len: number, max: number, min: number, step: number): number[] {
+    if(min < 1){
+        alert('min should be over than 0, min = 1');
+        min = 1;
+    }
+
+    if(max > 1000){
+        alert('max should be less than 1000, max = 1000');
+    }
+    
+    if(max < min){
+        alert('max is less than min, swapping max-min');
+        min += max;
+        max = min - max;
+        min -= max;
+    }
+
+    const generatedNumbers : number[] = [];
+    
+    for(let i : number = 0; i < len; i += step){
+        generatedNumbers.push(Math.floor(Math.random() * (max - min + 1) + min));
+    }
+
+    return generatedNumbers;
+}
---
+++ tests/tests.ts
@@ -0,0 +1,65 @@
+// Примеры тестов: простейшие короткие, случайные - 50 чисел, 100 чисел, 500 чисел, 1000 чисел, 
+// граничные - все числа 1 знака, все числа из 2х знаков, все числа из 3х знаков, 
+// каждого числа по 3 - всего чисел 900.
+
+describe('base', () => {
+    it('serialize-deserialize', () => {
+
+    });
+
+    it('source array', () => {
+
+    });
+
+    it('serialized string', () => {
+
+    });
+
+    it('is compression average over 50%', () => {
+
+    });
+});
+
+describe('array', () => {
+    it('is up to 1000', () => {
+
+    });
+
+    it('is 1 to 300', () => {
+
+    });
+});
+
+describe('functional', () => {
+    it('50 numbers', () => {
+
+    });
+
+    it('100 numbers', () => {
+
+    });
+
+    it('500 numbers', () => {
+
+    });
+
+    it('1000 numbers', () => {
+
+    });
+
+    it('numbers with one digit', () => {
+
+    });
+    
+    it('numbers with two digit', () => {
+
+    });
+
+    it('numbers with three digit', () => {
+
+    });
+
+    it('numbers with step over 3', () => {
+
+    });
+});
